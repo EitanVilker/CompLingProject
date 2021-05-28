85b6cf1014768125d997b5c03f112c681a780cc8
Kai Frey <kai.m.frey.22@dartmouth.edu>
2021-05-27 21:51:28 -0400

    updated csv to include all data

--- toCSV.py
+++ toCSV.py
@@ -1,3 +1,10 @@
+### Author: Kai Frey
+### Converts the raw text files from https://yeda.cs.technion.ac.il/resources_treebank.html 
+### into a csv to use as training data
+
+### Appends the data from the hebrewToTransliteration to the end of the csv. This is from 
+### https://yeda.cs.technion.ac.il/resources_lexicons_mila.html
+
 import csv
 
 # files to convert into csv
@@ -6,7 +13,7 @@ romanized = open('transliteratedRaw.txt', 'r')
 csv_id = 0
 
 with open('rawToCSV.csv', mode="w") as csv_file:
-  file_writer = csv.writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+  file_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
   # write the fields
   file_writer.writerow(['id', 'hebrew', 'transliterated'])
   # read one word from each file
@@ -22,6 +29,19 @@ with open('rawToCSV.csv', mode="w") as csv_file:
         file_writer.writerow([csv_id, h_words[i], r_words[i]])
         csv_id += 1   # increase the id by one
 
+  # Append the information from hebrewToTransliteration
+  with open('hebrewToTransliterated.csv', 'r') as lexicon:
+    lex_reader = csv.reader(lexicon, delimiter=',')
+    # get each row
+    header = 0 # acting as a true/false
+    for row in lex_reader:
+      if header == 0:
+        header = 1 # have made it past the header!
+        continue # get out of this iteration
+      # otherwise, add our id, the hebrew, & the transliteration
+      file_writer.writerow([csv_id, row[1], row[2]])
+      csv_id += 1
 
 hebrew.close()
 romanized.close()
+  
