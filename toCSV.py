c386d18a8636ede9f0b9d0a9b1d872c65ea7af2d
Kai Frey <kai.m.frey.22@dartmouth.edu>
2021-05-28 23:33:08 -0400

    created frequency corpus from TED

--- toCSV.py
+++ toCSV.py
@@ -1,6 +1,7 @@
 ### Author: Kai Frey
 ### Converts the raw text files from https://yeda.cs.technion.ac.il/resources_treebank.html 
-### into a csv to use as training data
+### into a csv to use as training data. 
+### Sections off phrases based on punctuation & new lines
 
 ### Appends the data from the hebrewToTransliteration to the end of the csv. This is from 
 ### https://yeda.cs.technion.ac.il/resources_lexicons_mila.html
@@ -16,18 +17,34 @@ with open('rawToCSV.csv', mode="w") as csv_file:
   file_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
   # write the fields
   file_writer.writerow(['id', 'hebrew', 'transliterated'])
-  # read one word from each file
+  h_curr = ""
+  r_curr = ""
+  # read from each line
   for h_line, r_line in zip(hebrew, romanized):
     # get the words
     h_words = h_line.split()
     r_words = r_line.split()
+    # current phrases
+    h_curr = ""
+    r_curr = ""
     for i in range(len(h_words)-1):
       # check that it is a useful item
       rtl_i = len(h_words)-1-i # get the right to left word at location i
-      if h_words[rtl_i].isalnum() and r_words[i].isalnum():
-        # same order as the other csv: id, hebrew, transliterated
-        file_writer.writerow([csv_id, h_words[i], r_words[i]])
+      # if it is a word, not punctuation, add to current phrase
+      if h_words[i].isalnum() and r_words[i].isalnum():
+        h_curr = (h_words[i] + " " + h_curr) # add right to left
+        r_curr += (r_words[i] + " ")
+      # otherwise, clean up the current phrase & write to the csv
+      elif h_curr != "" and r_curr != "":
+        file_writer.writerow([csv_id, h_curr.strip(), r_curr.strip()])
+        h_curr = ""
+        r_curr = ""
         csv_id += 1   # increase the id by one
+    # if at the end of line, add curr phrases
+    if h_curr != "" and r_curr != "":
+      file_writer.writerow([csv_id, h_curr.strip(), r_curr.strip()])
+      csv_id += 1
+      
 
   # Append the information from hebrewToTransliteration
   with open('hebrewToTransliterated.csv', 'r') as lexicon:
@@ -39,7 +56,7 @@ with open('rawToCSV.csv', mode="w") as csv_file:
         header = 1 # have made it past the header!
         continue # get out of this iteration
       # otherwise, add our id, the hebrew, & the transliteration
-      file_writer.writerow([csv_id, row[1], row[2]])
+      file_writer.writerow([csv_id, row[1].strip(), row[2].strip()])
       csv_id += 1
 
 hebrew.close()
---
+++ toCorpus.py
@@ -0,0 +1,32 @@
+### Author: Kai Frey
+### Coverts the raw text from https://github.com/ajinkyakulkarni14/TED-Multilingual-Parallel-Corpus/blob/master/Monolingual_data/Hebrew.txt
+### into a corpus with each word and the number of times it appears in the text
+
+import csv
+import string
+import re
+
+word_dict = {}
+w_id = 0
+# generate the dictionary first, then write to the csv
+with open('ted_hebrew.txt', 'r') as txt_file:
+  # run through every word
+  for line in txt_file:
+    # split and remove punctuation
+    words = line.split()
+    for w in words:
+      # remove all but hebrew letters
+      w = re.sub('[^\u05D0-\u05EA]+', '', w)
+      if w == '':
+        continue
+      if w not in word_dict.keys():
+        word_dict[w] = 1
+      else:
+        word_dict[w] += 1
+  # write csv
+  with open('occurrences.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['id', 'frequency', 'word'])
+    for word, freq in word_dict.items():
+      writer.writerow([w_id, word, freq])
+      w_id += 1
