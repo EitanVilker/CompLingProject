ce6c971590eb59357bb08a7662fed2b303b3faf4
Kai Frey <kai.m.frey.22@dartmouth.edu>
2021-06-05 15:02:11 -0400

    created rule dictionary

--- toCSV.py
+++ toCSV.py
@@ -1,4 +1,4 @@
-### Author: Kai Frey
+### Author: Kai Frey & Eitan Vilker
 ### Converts the raw text files from https://yeda.cs.technion.ac.il/resources_treebank.html 
 ### into a csv to use as training data. 
 ### Sections off phrases based on punctuation & new lines
@@ -7,13 +7,14 @@
 ### https://yeda.cs.technion.ac.il/resources_lexicons_mila.html
 
 import csv
+import re
 
 # files to convert into csv
 hebrew = open('hebrewRaw.txt', 'r')
 romanized = open('transliteratedRaw.txt', 'r')
 csv_id = 0
 
-with open('rawToCSV.csv', mode="w") as csv_file:
+with open('hebrewToTransliterated.csv', mode="w") as csv_file:
   file_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
   # write the fields
   file_writer.writerow(['id', 'hebrew', 'transliterated'])
@@ -29,10 +30,12 @@ with open('rawToCSV.csv', mode="w") as csv_file:
     r_curr = ""
     for i in range(len(h_words)-1):
       # check that it is a useful item
-      rtl_i = len(h_words)-1-i # get the right to left word at location i
       # if it is a word, not punctuation, add to current phrase
       if h_words[i].isalnum() and r_words[i].isalnum():
-        h_curr = (h_words[i] + " " + h_curr) # add right to left
+        # remove letters
+        h_curr = re.sub('[0-9]*', '', h_curr)
+        r_curr = re.sub('[0-9]*', '', r_curr)
+        h_curr += (h_words[i] + " ") # add right to left
         r_curr += (r_words[i] + " ")
       # otherwise, clean up the current phrase & write to the csv
       elif h_curr != "" and r_curr != "":
@@ -47,7 +50,7 @@ with open('rawToCSV.csv', mode="w") as csv_file:
       
 
   # Append the information from hebrewToTransliteration
-  with open('hebrewToTransliterated.csv', 'r') as lexicon:
+  with open('hebrew_translit_raw.csv', 'r') as lexicon:
     lex_reader = csv.reader(lexicon, delimiter=',')
     # get each row
     header = 0 # acting as a true/false
@@ -56,8 +59,12 @@ with open('rawToCSV.csv', mode="w") as csv_file:
         header = 1 # have made it past the header!
         continue # get out of this iteration
       # otherwise, add our id, the hebrew, & the transliteration
-      file_writer.writerow([csv_id, row[1].strip(), row[2].strip()])
-      csv_id += 1
+      # replace Arabic/punctuation with null
+      h_curr = re.sub('[\'؂U]+', '', row[1])
+      r_curr = re.sub(r"null", '', row[2])
+      if h_curr != '' and r_curr != '':
+        file_writer.writerow([csv_id, h_curr.strip(), r_curr.strip()])
+        csv_id += 1
 
 hebrew.close()
 romanized.close()
